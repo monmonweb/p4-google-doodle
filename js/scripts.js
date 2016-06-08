3f08b2e6f0cdb408271c374c26091c267f8b529f
monmonweb <mwebber1@it-mmc-m06.eastern.ewu.edu>
2016-06-08 13:37:27 -0700

    sdssfs

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
 var photo = document.getElementById("v");
-TweenMax.to(photo, 2, {opacity:"1", rotation:45});
+TweenMax.to(photo, 2, {opacity:"1", rotation:-360});
 
 var photo = document.getElementById("c");
-TweenMax.to(photo, 2, {opacity:"1", rotation:45});
+TweenMax.to(photo, 2, {opacity:"1", rotation:-360});
 
 var photo = document.getElementById("d");
-TweenMax.to(photo, 2, {opacity:"1", rotation:45});
+TweenMax.to(photo, 2, {opacity:"1", rotation:-360});
